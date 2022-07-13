b571370c7b87c8934117ef8fc7e0117faecb835e
heimonsy <heimonsy@gmail.com>
2022-07-12 22:17:32 -0600

    feat: add clix

---
+++ tools/clix/clix.go
@@ -0,0 +1,24 @@
+package clix
+
+import (
+	"context"
+
+	"github.com/heimonsy/micro/gen/proto/go/apis/user"
+	"google.golang.org/grpc"
+)
+
+func NewUserClient() (user.UserServiceClient, error) {
+	cli, err := grpc.DialContext(context.Background(), "micro-user:8080", grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+	return user.NewUserServiceClient(cli), nil
+}
+
+func NewRelationClient() (user.UserServiceClient, error) {
+	cli, err := grpc.DialContext(context.Background(), "micro-relation:8080", grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+	return user.NewUserServiceClient(cli), nil
+}
